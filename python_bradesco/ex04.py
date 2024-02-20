49bd062681df9cbe8acd7ef8e725e2d9d38255be
jpgbrito23 <158232568+jpgbrito23@users.noreply.github.com>
2024-02-20 15:03:52 -0300

    Delete python_bradesco/ex04.py

--- python_bradesco/ex04.py
+++
@@ -1,9 +0,0 @@
-prova1 = float(input("Digite a nota da primeira prova: "))
-prova2 = float(input("Digite a nota da segunda prova: "))
-
-media = (prova1+prova2)/2
-
-if media > 7:
-    print("Parabens vode foi aprovado sua média foi {}".format(media))
-else:
-    print("Infelizmente voce nao foi aprovado sua média foi {}".format(media))
